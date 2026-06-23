8fe99b1afeca0ecfaad4559d2f02c2a9bded9745
Kristoffer Jakobsson <kristoffer.tatel.jakobsson@gmail.com>
2026-06-23 14:42:45 +0200

    add stuff to drinkobject

--- Assets/Scripts/Runtime/Drink/DrinkObject.cs
+++ Assets/Scripts/Runtime/Drink/DrinkObject.cs
@@ -1,8 +1,19 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Runtime.Drink {
     public class DrinkObject : MonoBehaviour {
         public DrinkContents currentContents;
+
+        public void EmptyContents() {
+            currentContents.ingredients.Clear();
+            currentContents.mixType = MixType.None;
+        }
+
+        public void AddContents(DrinkContents contents) {
+            currentContents.ingredients.AddRange(contents.ingredients);
+            currentContents.mixType = contents.mixType;
+        }
         
         public void AddIngredient(Ingredient ingredient) {
             Debug.Log($"Added {ingredient.name}");
