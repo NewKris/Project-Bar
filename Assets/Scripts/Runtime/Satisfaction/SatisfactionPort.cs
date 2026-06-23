2a2d774ed8f197255407f9c121cda243a362f787
Tiffany Forsberg <tiff.forsberg@gmail.com>
2026-06-23 13:46:24 +0200

    Create satisfaction port

--- Assets/Scripts/Runtime/Satisfaction/SatisfactionPort.cs
+++ Assets/Scripts/Runtime/Satisfaction/SatisfactionPort.cs
@@ -1,10 +1,27 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Runtime.Satisfaction
 {
-    [CreateAssetMenu(fileName = "SatisfactionPort", menuName = "MENUNAME", order = 0)]
+    [CreateAssetMenu(fileName = "SatisfactionPort", menuName = "ScriptableObjects/SatisfactionPort")]
     public class SatisfactionPort : ScriptableObject
     {
-        
+        public UnityAction<int> OnSatisfactionChange;
+        public UnityAction<int> OnSatisfactionSet;
+
+        public void IncreaseSatisfaction(int value)
+        {
+            OnSatisfactionChange?.Invoke(Mathf.Abs(value));
+        }
+
+        public void DecreaseSatisfaction(int value)
+        {
+            OnSatisfactionChange?.Invoke(-Mathf.Abs(value));
+        }
+
+        public void SetSatisfaction(int value)
+        {
+            OnSatisfactionSet?.Invoke(value);
+        }
     }
 }
