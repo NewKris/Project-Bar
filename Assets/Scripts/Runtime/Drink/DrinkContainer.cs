6dad8d17c8b611302c6070e01958238285fca620
Kristoffer Jakobsson <kristoffer.tatel.jakobsson@gmail.com>
2026-06-23 14:38:07 +0200

    change drink content

---
+++ Assets/Scripts/Runtime/Drink/DrinkContainer.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace Runtime.Drink {
+    [CreateAssetMenu(menuName =  "Drink/Drink Container")]
+    public class DrinkContainer : ScriptableObject {
+        
+    }
+}
--- Assets/Scripts/Runtime/Drink/DrinkContents.cs
+++ Assets/Scripts/Runtime/Drink/DrinkContents.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 namespace Runtime.Drink {
     [Serializable]
     public struct DrinkContents {
+        public DrinkContainer container;
         public List<Ingredient> ingredients;
         public MixType mixType;
     }
