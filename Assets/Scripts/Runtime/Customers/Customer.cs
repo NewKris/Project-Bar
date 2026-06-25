88f47b6993eafc48fd8ae0c4ff267eae3763f089
Tiffany Forsberg <tiff.forsberg@gmail.com>
2026-06-25 13:16:59 +0200

    Start implementation of serving system

--- Assets/Scripts/Runtime/Customers/Customer.cs
+++ Assets/Scripts/Runtime/Customers/Customer.cs
@@ -20,7 +20,7 @@ namespace Runtime.Customers
         [SerializeField] private bool isTarget;
         
         [Tooltip("The possible drinks that when served to the customer will result in a successful order")]
-        [SerializeField] private List<DrinkContents> drinks;
+        [SerializeField] private List<Recipe> acceptableDrinks;
         
         
         [Foldout("Timers")]
@@ -92,7 +92,108 @@ namespace Runtime.Customers
         public void ServeDrink(DrinkContents drink)
         {
             // Compare contents with accepted drinks
-            Debug.Log("Served");
+            Debug.Log("Serving 💅");
+
+            if (IsDrinkAccepted(drink))
+            {
+                Debug.Log("Drink accepted!");
+                _customerDialogue.Success();
+                satisfactionPort.IncreaseSatisfaction(satisfactionSuccess);
+            }
+            else
+            {
+                Debug.Log("Drink rejected");
+                _customerDialogue.Failure();
+                satisfactionPort.DecreaseSatisfaction(satisfactionFailure);
+            }
+            
+            LeaveBar();
+        }
+
+        private bool IsDrinkAccepted(DrinkContents drink)
+        {
+            List<Recipe> possibleRecipes = acceptableDrinks;
+            
+            possibleRecipes = CheckForMatchingContainer(drink.container, possibleRecipes);
+            if (possibleRecipes.Count == 0)
+            {
+                Debug.Log("Drink mismatch: Container");
+                return false;
+            }
+            
+            possibleRecipes = CheckForMatchingMixType(drink.mixType, possibleRecipes);
+            if (possibleRecipes.Count == 0)
+            {
+                Debug.Log("Drink mismatch: MixType");
+                return false;
+            }
+            
+            possibleRecipes = CheckForMatchingIngredients(drink.ingredients, possibleRecipes);
+            if (possibleRecipes.Count == 0)
+            {
+                Debug.Log("Drink mismatch: Ingredients");
+                return false;
+            }
+
+            return CheckForCorrectOrderOfIngredients(drink.ingredients);
+        }
+
+        private List<Recipe> CheckForMatchingContainer(DrinkContainer container, List<Recipe> recipes)
+        {
+            List<Recipe> possibleRecipes = new List<Recipe>();
+
+            foreach (Recipe recipe in recipes)
+            {
+                if (recipe.contents.container == container) possibleRecipes.Add(recipe);
+            }
+
+            return possibleRecipes;
+        }
+
+        private List<Recipe> CheckForMatchingMixType(MixType mixType, List<Recipe> recipes)
+        {
+            List<Recipe> possibleRecipes = new List<Recipe>();
+
+            foreach (Recipe recipe in recipes)
+            {
+                if (recipe.contents.mixType == mixType) possibleRecipes.Add(recipe);
+            }
+
+            return possibleRecipes;
+        }
+
+        private List<Recipe> CheckForMatchingIngredients(List<Ingredient> ingredients, List<Recipe> recipes)
+        {
+            List<Recipe> possibleRecipes = new List<Recipe>();
+
+            foreach (Recipe recipe in recipes)
+            {
+                bool isPossible = true;
+                foreach (Ingredient ingredient in ingredients)
+                {
+                    if (!recipe.contents.ingredients.Contains(ingredient))
+                    {
+                        isPossible = false;
+                        break;
+                    }
+                }
+                
+                if (isPossible) possibleRecipes.Add(recipe);
+            }
+            
+            return possibleRecipes;
+        }
+
+        private bool CheckForCorrectOrderOfIngredients(List<Ingredient> ingredients)
+        {
+            IngredientType typeOfPreviousIngredient = ingredients[0].type;
+
+            for (int i = 1; i < ingredients.Count; i++)
+            {
+                if (ingredients[i].type < typeOfPreviousIngredient) return false;
+            }
+
+            return true;
         }
 
         public void Order()
