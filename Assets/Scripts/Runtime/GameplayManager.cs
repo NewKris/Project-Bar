cfa47a8ad4f2ba2f5c55f5ecf05419df083507da
Kristoffer Jakobsson <kristoffer.tatel.jakobsson@gmail.com>
2026-07-06 15:40:04 +0200

    add pseudo pause menu

--- Assets/Scripts/Runtime/GameplayManager.cs
+++ Assets/Scripts/Runtime/GameplayManager.cs
@@ -19,5 +19,15 @@ namespace Runtime {
         private void TriggerGameOver() {
             sceneHandler.GameOver();
         }
+
+        private void OnGUI() {
+            GUILayout.BeginArea(new Rect(Screen.width - 110, 10, 100, 300));
+            
+            if (GUILayout.Button("Exit Game")) {
+                sceneHandler.MainMenu();
+            }
+            
+            GUILayout.EndArea();
+        }
     }
 }
