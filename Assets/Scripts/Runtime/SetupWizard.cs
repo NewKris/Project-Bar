7882ed673c37e2b0d2d5f467b8432e2cd6c295f9
Kristoffer Jakobsson <82028896+NewKris@users.noreply.github.com>
2026-07-15 12:44:17 +0200

    create more wizard buttons

--- Assets/Scripts/Runtime/SetupWizard.cs
+++ Assets/Scripts/Runtime/SetupWizard.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 namespace Runtime {
     public class SetupWizard : MonoBehaviour {
         public Color promptColor;
+        public float promptSize;
         
         [Button]
-        public void ResetPromptColors() {
+        public void ResetPromptDebugColors() {
             foreach (LookPrompt prompt in FindObjectsByType<LookPrompt>(FindObjectsInactive.Include, FindObjectsSortMode.None)) {
                 prompt.debugColor  = promptColor;
             }
-
-            
+        }
+        
+        [Button]
+        public void ResetPromptDebugSize() {
+            foreach (LookPrompt prompt in FindObjectsByType<LookPrompt>(FindObjectsInactive.Include, FindObjectsSortMode.None)) {
+                prompt.debugSize  = promptSize;
+            }
         }
     }
 }
