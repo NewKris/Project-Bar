e0fd9d23844541ac5c8dd3eff16ac66c069cab81
Tiffany Forsberg <tiff.forsberg@gmail.com>
2026-07-29 11:57:29 +0200

    Add final handles for customer slots

--- Assets/Scripts/Editor/CustomerSlotEditor.cs
+++ Assets/Scripts/Editor/CustomerSlotEditor.cs
@@ -14,7 +14,10 @@ namespace Editor {
                 slot.customerSpawnPosition,
                 slot.spawnPositionHandleColors.YAxisColor,
                 slot.spawnPositionHandleColors.XAxisColor,
-                slot.spawnPositionHandleColors.ZAxisColor
+                slot.spawnPositionHandleColors.ZAxisColor,
+                slot.spawnPositionHandleColors.XYPlaneColor,
+                slot.spawnPositionHandleColors.YZPlaneColor,
+                slot.spawnPositionHandleColors.XZPlaneColor
             );
             
             slot.customerOrderPosition = CreatePositionHandles
@@ -22,7 +25,10 @@ namespace Editor {
                 slot.customerOrderPosition,
                 slot.orderPositionHandleColors.YAxisColor,
                 slot.orderPositionHandleColors.XAxisColor,
-                slot.orderPositionHandleColors.ZAxisColor
+                slot.orderPositionHandleColors.ZAxisColor,
+                slot.orderPositionHandleColors.XYPlaneColor,
+                slot.orderPositionHandleColors.YZPlaneColor,
+                slot.orderPositionHandleColors.XZPlaneColor
             );
             
             slot.customerExitPosition = CreatePositionHandles
@@ -30,13 +36,16 @@ namespace Editor {
                 slot.customerExitPosition,
                 slot.exitPositionHandleColors.YAxisColor,
                 slot.exitPositionHandleColors.XAxisColor,
-                slot.exitPositionHandleColors.ZAxisColor
+                slot.exitPositionHandleColors.ZAxisColor,
+                slot.exitPositionHandleColors.XYPlaneColor,
+                slot.exitPositionHandleColors.YZPlaneColor,
+                slot.exitPositionHandleColors.XZPlaneColor
             );
             
             
         }
 
-        private Vector3 CreatePositionHandles(Vector3 position, Color yColor, Color xColor, Color zColor) {
+        private Vector3 CreatePositionHandles(Vector3 position, Color yColor, Color xColor, Color zColor, Color xyColor, Color zyColor, Color xzColor) {
             Vector3 newPosition = position;
             
             Handles.color = yColor;
@@ -47,6 +56,46 @@ namespace Editor {
 
             Handles.color = zColor;
             newPosition = Handles.Slider(newPosition, Vector3.forward);
+            
+            Handles.color = xyColor;
+            newPosition = Handles.Slider2D(
+                0,
+                newPosition, 
+                (Vector3.right + Vector3.up).normalized,
+                Vector3.forward, 
+                Vector3.right,
+                Vector3.up,
+                0.5f,
+                Handles.RectangleHandleCap,
+                new Vector2(0.2f, 0.2f)
+            );
+            
+            Handles.color = zyColor;
+            newPosition = Handles.Slider2D(
+                1,
+                newPosition, 
+                (Vector3.forward + Vector3.up).normalized,
+                Vector3.right, 
+                Vector3.forward,
+                Vector3.up,
+                0.5f,
+                Handles.RectangleHandleCap,
+                new Vector2(0.2f, 0.2f)
+            );
+            
+            Handles.color = xzColor;
+            newPosition = Handles.Slider2D(
+                2,
+                newPosition,
+                 
+                (Vector3.right + Vector3.forward).normalized,
+                Vector3.up, 
+                Vector3.right,
+                Vector3.forward,
+                0.5f,
+                Handles.RectangleHandleCap,
+                new Vector2(0.2f, 0.2f)
+            );
 
             return newPosition;
         }
--- Assets/Scripts/Runtime/CustomHandleColors.cs
+++ Assets/Scripts/Runtime/CustomHandleColors.cs
@@ -7,9 +7,15 @@ namespace Runtime {
         public Color YAxisColor => new Color(yAxisHandleColor.r, yAxisHandleColor.g, yAxisHandleColor.b, 1f);
         public Color XAxisColor => new Color(xAxisHandleColor.r, xAxisHandleColor.g, xAxisHandleColor.b, 1f);
         public Color ZAxisColor => new Color(zAxisHandleColor.r, zAxisHandleColor.g, zAxisHandleColor.b, 1f);
+        public Color XYPlaneColor => new Color(xyPlaneHandleColor.r, xyPlaneHandleColor.g, xyPlaneHandleColor.b, 1f);
+        public Color YZPlaneColor => new Color(yzPlaneHandleColor.r, yzPlaneHandleColor.g, yzPlaneHandleColor.b, 1f);
+        public Color XZPlaneColor => new Color(xzPlaneHandleColor.r, xzPlaneHandleColor.g, xzPlaneHandleColor.b, 1f);
         
         [ColorUsage(false, false)] [SerializeField] private Color yAxisHandleColor;
         [ColorUsage(false, false)] [SerializeField] private Color xAxisHandleColor;
         [ColorUsage(false, false)] [SerializeField] private Color zAxisHandleColor;
+        [ColorUsage(false, false)] [SerializeField] private Color xyPlaneHandleColor;
+        [ColorUsage(false, false)] [SerializeField] private Color yzPlaneHandleColor;
+        [ColorUsage(false, false)] [SerializeField] private Color xzPlaneHandleColor;
     }
 }
